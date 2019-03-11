538935baa6820840b1a9cd6e14ca8e67dabf77b3
jbuisson <47634012+jbuisson@users.noreply.github.com>
2019-03-11 17:00:57 +0100

    update driver IE

--- libreplan1.4.1/src/test/java/grp1/autom1/libreplan/junit/ProjectAndTaskTest.java
+++ libreplan1.4.1/src/test/java/grp1/autom1/libreplan/junit/ProjectAndTaskTest.java
@@ -56,7 +56,7 @@ public class ProjectAndTaskTest {
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\formation\\chromedriver.exe");
 	 //  driver = new ChromeDriver();
 
-	//	driver.get("http://localhost:8080/libreplan");
+				driver.get("http://localhost:8080/libreplan");
 
 
 	}
